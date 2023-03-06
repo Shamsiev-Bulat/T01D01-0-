5b0f54260498d764626639182090852dc78af296
Tumberjon Rubi <tumberjr@ev-b2.kzn.21-school.ru>
2023-03-06 17:41:57 +0300

    Day 01 done

---
+++ src/quest3.sh
@@ -0,0 +1,92 @@
+10  cd T01D01-0
+   11  cd /usr
+   12  ls -l
+   13  cd .
+   14  cd ..
+   15  cd 
+   16  ls -l
+   17  cd T01D01-0
+   18  ls -l
+   19  cd /src
+   20  cd scr
+   21  cd src
+   22  sl -l
+   23  ls --l
+   24  ls -l
+   25  chmod 777 ai_initial_module.sh
+   26  ls all
+   27  ls -all
+   28  ./ai_initial_module.sh
+   29  ./ai_initial_module.sh
+   30  ./ai_initial_module.sh
+   31  ls -l
+   32  chmod 777 ai_module_2.sh
+   33  ./ai_initial_module.sh
+   34  ls -l
+   35  chown * 
+   36  chmod 777 * 
+   37  ls -l
+   38  ./ai_initial_module.sh
+   39  ./ai_module_2.sh
+   40  ls -l 
+   41  vim important_data_for_ai_module_2.txt
+   42  ./ai_initial_module.sh
+   43  git add ai_initial_module.sh
+   44  git add ai_module_2.sh
+   45  git add important_data_for_ai_module_2.txt
+   46  git status
+   47  cd 
+   48  ls -l
+   49  cd T01D01-0
+   50  git checkout -b develop
+   51  git branch 
+   52  git status
+   53  cd src
+   54  ./ai_door_management_module.sh
+   55  ls -l
+   56  ld 
+   57  ls 
+   58  ls -l
+   59  ls -ln
+   60  ls -la
+   61  ./ai_door_management_module.sh
+   62  mv door_management_files
+   63  mv door_management_fi door_management_files
+   64  ./ai_door_management_module.sh
+   65  mkdir door_configuration
+   66  mkdir door_map
+   67  mkdir door_logs
+   68  git branch
+   69  ls -l
+   70  cd door_management_files
+   71  ls -l
+   72  mkdir door_configuration
+   73  ls -l
+   74  ls -l
+   75  cp door_*.conf ~/T01D01-0/src/door_management_files/door_configuration
+   76  cd door_configuration
+   77  ls -l
+   78  cd ..
+   79  mkdir door_logs
+   80  cp door_*.conf ~/T01D01-0/src/door_management_files/door_logs
+   81  cp door_*.log ~/T01D01-0/src/door_management_files/door_logs
+   82  ls -l
+   83  mdkir door_map
+   84  mkdir door_map
+   85  cp door_map_1.1 ~/T01D01-0/src/door_management_files/door_map
+   86  cd ..
+   87  ls -ln
+   88  ./ai_door_management_module.sh
+   89  ./ai_door_management_module.sh
+   90  cd T01D01-0
+   91  cd src
+   92  ls -l
+   93  ./ai_door_management_module.sh
+   94  git branch
+   95  git status
+   96  cd T01D01-0
+   97  git branch
+   98  git commit -m "refactor" 
+   99  git push -u origin develop
+  100  vim quest3.sh
+
---
+++ src/quest6.sh
@@ -0,0 +1,43 @@
+147  cd ..
+  148  ls -l
+  149  ./ai_door_management_module.sh
+  150  ls
+  151  cd T01D01-0
+  152  cd src
+  153  cat ./ai_help
+  154  cat ./ai_hel
+  155  ls ./ai_hel
+  156  cd ai_hel
+  157  ls -l
+  158  ai_help ls
+  159  ps
+  160  ls -ln
+  161  cd ai_help
+  162  ls -l
+  163  chmod 777 *
+  164  ./keygen.sh
+  165  ls -l
+  166  cd key
+  167  ls -l
+  168  rm -R file*
+  169  ls -ln
+  170  cd ..
+  171  ls -ln
+  172  cd unifier.sh
+  173   ./unifier.sh
+  174  cd key
+  175  ls -ln
+  176   ./unifier.sh
+  177  ./unifier.sh
+  178  cd 
+  179  ls -ln
+  180  cd T01D01-0
+  181  cd src
+  182  ls -ln
+  183  cd ai_help
+  184  ls -ln
+  185  cd ..
+  186  ls -ln
+  187  cat quest4.sh
+  188  history
+  189  history 167-187
